924858f8dadf2d4f0c4a1b27938e2dafb470bdbe
Nur0606 <seidekmatov@gmail.com>
2024-04-05 15:53:32 +0600

    department logoka nur,nurti

--- src/service/serviceImpl/DepartmentServiceImpl.java
+++ src/service/serviceImpl/DepartmentServiceImpl.java
@@ -1,4 +1,21 @@
 package service.serviceImpl;
 
-public class DepartmentServiceImpl {
+import dao.DepartmentDao;
+import dao.daoImpl.DepartmentDaoImpl;
+import models.Department;
+import service.DepartmentService;
+
+import java.util.List;
+
+public class DepartmentServiceImpl implements DepartmentService {
+    DepartmentDao dao = new DepartmentDaoImpl();
+    @Override
+    public List<Department> getAllDEaprtment() {
+        return dao.getAllDEaprtment();
+    }
+
+    @Override
+    public String deleteDepartmentIdByHospitalId(Long hospitalId, Long departmentId) {
+        return dao.deleteDepartmentIdByHospitalId(hospitalId, departmentId);
+    }
 }
